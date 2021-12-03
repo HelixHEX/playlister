60e047c4cb4974062da3c60b8024f7c9549e263b
Elias <e.wambugu192@gmail.com>
2021-12-03 15:33:45 -0800

    Users can see comments

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 
 app = Flask(__name__)
 
+
 def video_url_creator(id_lst):
     videos = []
     for vid_id in id_lst:
@@ -13,19 +14,25 @@ def video_url_creator(id_lst):
         videos.append(video)
     return videos
 
-client = MongoClient("mongodb+srv://heroku:1234@cluster0.f7pwd.mongodb.net/Cluster0?retryWrites=true&w=majority")
+
+client = MongoClient(
+    "mongodb+srv://heroku:1234@cluster0.f7pwd.mongodb.net/Cluster0?retryWrites=true&w=majority")
 db = client.Playlister
 playlists = db.playlists
+comments = db.comments
+
 
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists.find())
 
+
 @app.route('/playlists/new')
 def playlists_new():
     """Create a new playlist."""
     return render_template('playlists_new.html', playlist={}, title="New Playlist")
+
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
@@ -43,11 +50,13 @@ def playlists_submit():
     playlists.insert_one(playlist)
     return redirect(url_for('playlists_index'))
 
+
 @app.route('/playlists/<playlist_id>')
 def playlists_show(playlist_id):
     """Show a single playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    return render_template('playlists_show.html', playlist=playlist)
+    playlist_comments = comments.find({'playlist_id': playlist_id})
+    return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
 
 @app.route('/playlists/<playlist_id>/edit')
 def playlists_edit(playlist_id):
@@ -55,6 +64,7 @@ def playlists_edit(playlist_id):
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     # Add the title parameter here
     return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
 
 @app.route('/playlists/<playlist_id>', methods=['POST'])
 def playlists_update(playlist_id):
@@ -75,11 +85,33 @@ def playlists_update(playlist_id):
     # take us back to the playlist's show page
     return redirect(url_for('playlists_show', playlist_id=playlist_id, title='Edit Playlist'))
 
+
 @app.route('/playlists/<playlist_id>/delete', methods=['POST'])
 def playlists_delete(playlist_id):
     """Delete one playlist."""
     playlists.delete_one({'_id': ObjectId(playlist_id)})
     return redirect(url_for('playlists_index'))
+
+# Add this header to distinguish Comment routes from Playlist routes
+########## COMMENT ROUTES ##########
+
+
+@app.route('/playlists/comments', methods=['POST'])
+def comments_new():
+    """Submit a new comment."""
+    playlist_id = request.form.get("playlist_id")
+    comment = {
+        "title": request.form.get("title"),
+        "content": request.form.get("content"),
+        "playlist_id": playlist_id
+    }
+    comments.insert_one(comment)
+    all_comments = comments.find()
+    for commenti in all_comments:
+        print(commenti)
+    print(all_comments)
+    return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
